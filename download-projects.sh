61925cc923b8a1864d9336f4ed7cbc53682430af
Chi-teck <Chi-teck@users.noreply.github.com>
2017-10-15 18:00:06 +0500

    Updated drupal projects data.

--- download-projects.sh
+++ download-projects.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-mkdir .drupal-projects
+mkdir drupal-projects
 
-for page in {0..99}; do
-  echo PAGE $page
+for PAGE in {0..99}; do
+  echo PAGE $PAGE
   echo =========================================
-  urls=$(curl -s https://www.drupal.org/project/usage?page=$page | awk '/<td><a href="\/project/ { split($0, a, "\""); print "https://www.drupal.org/"a[4];}');
+  urls=$(curl -s https://www.drupal.org/project/usage?page=$PAGE | awk '/<td><a href="\/project/ { split($0, a, "\""); print "https://www.drupal.org/"a[4];}');
   for url in $urls; do
-    html=$(curl -s $url);
-    project=${url/*\// }
-    if [ $project != drupal ]; then
-      echo Checking $project...
+    HTML=$(curl -s $url);
+    PROJECT=${url/*\// }
+    if [ $PROJECT != drupal ]; then
+      echo Checking $PROJECT...
       for i in {6,7,8}; do
-        found=$(echo "$html" | grep "<td class=\"active\"><a href=\"/project/usage/.*\">$i.x")
-        if [ -n "$found" ]; then
-          echo $project >> drupal-projects/d$i.txt
+        FOUND=$(echo "$HTML" | grep "<td class=\"active\"><a href=\"/project/usage/.*\">$i.x")
+        if [ -n "$FOUND" ]; then
+          echo $PROJECT >> drupal-projects/d$i.txt
         fi
       done
     fi
