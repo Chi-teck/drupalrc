4ed22325ce7b2f25b8555e278bd805d19a2686a1
Chi-teck <Chi-teck@users.noreply.github.com>
2017-10-15 11:38:27 +0500

    Toggled case of Bash variables.

--- drupal.complete.sh
+++ drupal.complete.sh
@@ -9,18 +9,18 @@ _dcd() {
     return
   fi
 
-  local drupal_root=$(droot)
-  if [ -z "$drupal_root" ]; then
+  local DRUPAL_ROOT=$(droot)
+  if [ -z "$DRUPAL_ROOT" ]; then
     return 1
   fi
 
-  local suggestions
-  for dir in $(_dcd_dirs $(_dversion $drupal_root)); do
-    [ -d $drupal_root/$dir ] && suggestions="$suggestions "$(find $drupal_root/$dir -maxdepth 1 -type d -name '[^.]*' -exec basename {} \;)
+  local SUGGESTIONS
+  for dir in $(_dcd_dirs $(_dversion $DRUPAL_ROOT)); do
+    [ -d $DRUPAL_ROOT/$dir ] && SUGGESTIONS="$SUGGESTIONS "$(find $DRUPAL_ROOT/$dir -maxdepth 1 -type d -name '[^.]*' -exec basename {} \;)
   done
 
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "${suggestions}" -- ${cur}) )
+  local CUR="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "${SUGGESTIONS}" -- ${CUR}) )
 }
 complete -F _dcd dcd
 
@@ -30,24 +30,24 @@ complete -F _dcd dcd
 _dl() {
   COMPREPLY=()
 
-  local drupal_root=$(droot)
-  if [ -z "$drupal_root" ]; then
+  local DRUPAL_ROOT=$(droot)
+  if [ -z "$DRUPAL_ROOT" ]; then
     return 1
   fi
 
-  local drupal_version=$(_dversion $drupal_root)
+  local DRUPAL_VERSION=$(_dversion $DRUPAL_ROOT)
 
-  local suggestions  
-  if [ -f $HOME/.drupal-projects/d$drupal_version.txt ]; then
-      suggestions=$(cat $HOME/.drupal-projects/d$drupal_version.txt)
-  elif [ -f /usr/share/drupal-projects/d$drupal_version.txt ]; then
-      suggestions=$(cat /usr/share/drupal-projects/d$drupal_version.txt)
+  local SUGGESTIONS
+  if [ -f $HOME/.drupal-projects/d$DRUPAL_VERSION.txt ]; then
+      SUGGESTIONS=$(cat $HOME/.drupal-projects/d$DRUPAL_VERSION.txt)
+  elif [ -f /usr/share/drupal-projects/d$DRUPAL_VERSION.txt ]; then
+      SUGGESTIONS=$(cat /usr/share/drupal-projects/d$DRUPAL_VERSION.txt)
   else  
     return 1
   fi
 
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "${suggestions}" -- ${cur}) )
+  local CUR="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "${SUGGESTIONS}" -- ${CUR}) )
 }
 complete -F _dl dl
 complete -F _dl dle
@@ -63,15 +63,15 @@ _dconf() {
     return
   fi
 
-  local drupal_root=$(droot)
-  if [ -z "$drupal_root" ]; then
+  local DRUPAL_ROOT=$(droot)
+  if [ -z "$DRUPAL_ROOT" ]; then
     return 1
   fi
 
-  local suggestions=$(find $drupal_root/sites/default -maxdepth 1 -type f -exec basename {} \;)
+  local SUGGESTIONS=$(find $DRUPAL_ROOT/sites/default -maxdepth 1 -type f -exec basename {} \;)
 
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "${suggestions}" -- ${cur}) )
+  local CUR="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "${SUGGESTIONS}" -- ${CUR}) )
 }
 complete -F _dconf dconf
 
@@ -86,14 +86,14 @@ _dbin() {
     return
   fi
 
-  local drupal_root=$(droot)
-  if [ -z "$drupal_root" ]; then
+  local DRUPAL_ROOT=$(droot)
+  if [ -z "$DRUPAL_ROOT" ]; then
     return 1
   fi
 
-  local suggestions=$(find $(_dvendor $drupal_root)/bin -mindepth 1 -maxdepth 1 -executable -exec basename {} \;)
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  COMPREPLY=( $(compgen -W "${suggestions}" -- ${cur}) )
+  local SUGGESTIONS=$(find $(_dvendor $DRUPAL_ROOT)/bin -mindepth 1 -maxdepth 1 -executable -exec basename {} \;)
+  local CUR="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "${SUGGESTIONS}" -- ${CUR}) )
 }
 complete -F _dbin dbin
 
@@ -103,24 +103,24 @@ complete -F _dbin dbin
 _dtest() {
   COMPREPLY=()
 
-  local drupal_root=$(droot)
-  if [ -z "$drupal_root" ]; then
+  local DRUPAL_ROOT=$(droot)
+  if [ -z "$DRUPAL_ROOT" ]; then
     return 1
   fi
 
-  local cur="${COMP_WORDS[COMP_CWORD]}"
-  local suggestions
-  local file=$drupal_root/$([ $(_dversion $drupal_root) = 8 ] && echo 'core/')scripts/run-tests.sh
+  local CUR="${COMP_WORDS[COMP_CWORD]}"
+  local SUGGESTIONS
+  local FILE=$DRUPAL_ROOT/$([ $(_dversion $DRUPAL_ROOT) = 8 ] && echo 'core/')scripts/run-tests.sh
 
   # Options.
-  if [ "${cur:0:1}" = '-' ];then
-    suggestions=$(php $file --help | grep -oE ' --[a-z-]+')
+  if [ "${CUR:0:1}" = '-' ];then
+    SUGGESTIONS=$(php $FILE --help | grep -oE ' --[a-z-]+')
   # Groups.
   else
-    suggestions=$(php $file --list | grep -vE '([- ]|^$)')
+    SUGGESTIONS=$(php $FILE --list | grep -vE '([- ]|^$)')
   fi
 
-  COMPREPLY=( $(compgen -W "${suggestions}" -- ${cur}) )
+  COMPREPLY=( $(compgen -W "${SUGGESTIONS}" -- ${CUR}) )
 }
 complete -F _dtest dtest
 
@@ -131,23 +131,23 @@ complete -F _dtest dtest
 _dunit() {
   COMPREPLY=()
 
-  local drupal_root=$(droot)
-  if [ -z "$drupal_root" ]; then
+  local DRUPAL_ROOT=$(droot)
+  if [ -z "$DRUPAL_ROOT" ]; then
     return 1
   fi
 
-  local file=$(_dvendor $drupal_root)/bin/phpunit
-  if [ ! -f $file ]; then
+  local FILE=$(_dvendor $DRUPAL_ROOT)/bin/phpunit
+  if [ ! -f $FILE ]; then
     return 1
   fi
 
-  local cur="${COMP_WORDS[COMP_CWORD]}"
+  local CUR="${COMP_WORDS[COMP_CWORD]}"
   # Only options can be completed.
-  if [ "${cur:0:1}" != '-' ];then
+  if [ "${CUR:0:1}" != '-' ];then
     return 0
   fi
 
-  local suggestions=$($file --help | grep -oE ' --[a-z-]+')
-  COMPREPLY=( $(compgen -W "${suggestions}" -- ${cur}) )
+  local SUGGESTIONS=$($FILE --help | grep -oE ' --[a-z-]+')
+  COMPREPLY=( $(compgen -W "${SUGGESTIONS}" -- ${CUR}) )
 }
 complete -fF _dunit dunit
